1786a098f0de677e2ce3243d3749551c26aced9a
Nipa Dev <60810623+Nipa-Code@users.noreply.github.com>
2023-07-31 16:40:47 +0300

    Bump postgres from 12 to 15 and fix connection to database or smth

--- lemonapi/utils/config.py
+++ lemonapi/utils/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     env_name: str = "Local"
     base_url: str = f"http://localhost:{port}"
     sqlite_url: str = "sqlite:///./shortener.db"
-    postgres_url: str = f"postgresql+psycopg2://{config('DB_USER')}:{config('DB_PASSWORD')}@postgres/{config('DB_NAME')}"
+    postgres_url: str = f"postgresql+psycopg2://{config('DB_USER')}:{config('DB_PASSWORD')}@postgres:{config('DB_PORT')}/{config('DB_NAME')}"
 
     class Config:
         env_file = ".env"
