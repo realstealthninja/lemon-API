b968f001c164e564f7d1c719319d21632078f858
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-05-11 19:21:58 +0530

    feat: add a test for / endpoint

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,9 +8,21 @@ client = TestClient(app)
 def test_server_status():
     response = client.get("/status/")
     assert response.status_code == 200
-    assert response.content ==  "Server is running."
+    assert response.text ==  "Server is running."
 
 def test_server_metrics():
     response = client.get("/metrics/")
     assert response.status_code == 200
+
+def test_home_endpoint():
+    root = client.get("/")
+    docs = client.get("/docs/")
+
+    assert root.status_code == 200
+    assert root.content == docs.content
+
+def test_uknown_endpoint():
+    response = client.get("/asldkfalksdjf/")
+    assert response.status_code == 404
+    
     
