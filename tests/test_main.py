45338ba42234cf9bbd2c27628c780c124293e9e6
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-05-11 12:41:08 +0530

    fix: add assert for content and add a new test for metrics

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,3 +8,9 @@ client = TestClient(app)
 def test_server_status():
     response = client.get("/status/")
     assert response.status_code == 200
+    assert response.content ==  "Server is running."
+
+def test_server_metrics():
+    response = client.get("/metrics/")
+    assert response.status_code == 200
+    
